9e83eb2346def7e355ea59a4c9b3e7b3c70dd000
Ofer Tlusty <ofertlusty@mail.tau.ac.il>
2021-06-01 11:53:49 +0300

    set prints with numpy.round

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -75,7 +75,8 @@ def printOutput(centroids):
     str = ""
     for i in range(len(centroids)): 
         for d in range(len(centroids[0])): 
-            str += "{}".format(format(centroids[i][d], '.4f'))
+            # str += "{}".format(format(centroids[i][d], '.4f')) # rounding with format
+            str += "{}".format(np.round(centroids[i][d], 4)) # rounding with np.round
             str += ","
         str = str[:-1] + "\n"
     
